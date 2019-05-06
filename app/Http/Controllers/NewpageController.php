67cd465f4d90b5ebac9f6643f6ff1f22dd047b21
NS <swopown@yandex.ru>
2019-05-06 19:53:56 +0300

    Laravel market

---
+++ app/Http/Controllers/NewpageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Newpage extends Controller
+{
+    public function index()
+    {
+        var_dump('ok');
+    }
+}
